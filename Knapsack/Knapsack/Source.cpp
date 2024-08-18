310f949dfb85da51aab575ea2a3a1f286f9a9e1e
Dainn <dangtang2012@gmail.com>
2024-08-18 10:11:49 +0700

    update file

--- Knapsack/Knapsack/Source.cpp
+++ Knapsack/Knapsack/Source.cpp
@@ -6,47 +6,33 @@ int max(int a, int b) {
 	return (a > b) ? a : b;
 }
 
-// Hàm giải bài toán Balo 0/1 bằng quy hoạch động
-int knapsack(int W, int weights[], int values[], int n) {
-	int i, w;
-	// Cấp phát động bảng K
-	int **K = (int **)malloc((n + 1) * sizeof(int *));
-	for (i = 0; i <= n; i++) {
-		K[i] = (int *)malloc((W + 1) * sizeof(int));
+// Hàm giải bài toán Balo không giới hạn bằng quy hoạch động
+int unboundedKnapsack(int W, int weights[], int values[], int n) {
+	int *dp = (int *)malloc((W + 1) * sizeof(int));
+
+	// Khởi tạo dp với giá trị 0
+	for (int i = 0; i <= W; i++) {
+		dp[i] = 0;
 	}
 
-	// Xây dựng bảng K[][] theo kiểu bottom-up
-	for (i = 0; i <= n; i++) {
-		for (w = 0; w <= W; w++) {
-			if (i == 0 || w == 0) {
-				K[i][w] = 0;
-			}
-			else if (weights[i - 1] <= w) {
-				K[i][w] = max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
-			}
-			else {
-				K[i][w] = K[i - 1][w];
-			}
+	// Xây dựng bảng dp[] theo kiểu bottom-up
+	for (int i = 0; i < n; i++) {
+		for (int w = weights[i]; w <= W; w++) {
+			dp[w] = max(dp[w], dp[w - weights[i]] + values[i]);
 		}
 	}
 
-	int result = K[n][W];
-
-	// Giải phóng bộ nhớ của bảng K
-	for (i = 0; i <= n; i++) {
-		free(K[i]);
-	}
-	free(K);
-
+	int result = dp[W];
+	free(dp);
 	return result;
 }
 
 int main() {
-	int values[] = { 60, 100, 120 };
-	int weights[] = { 10, 20, 30 };
+	int values[] = { 60, 100, 140, 140 };
+	int weights[] = { 10, 20, 30, 50 };
 	int W = 50;
 	int n = sizeof(values) / sizeof(values[0]);
 
-	printf("Gia tri lon nhat co the dat duoc la: %d\n", knapsack(W, weights, values, n));
+	printf("Gia tri lon nhat co the dat duoc la: %d\n", unboundedKnapsack(W, weights, values, n));
 	return 0;
 }
